01245695b1f423a96cee3f267176864005464201
mafengyan <2937735461@qq.com>
2020-11-01 15:21:22 +0800

    2020-11-01

---
+++ 算法/leetcode/面试宝典/面试题 02.06. 回文链表/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0.0
+ * @Author: Mfy
+ * @Date: 2020-11-01 15:20:44
+ * @LastEditors: Mfy
+ * @LastEditTime: 2020-11-01 15:20:53
+ */
+/**
+ * 编写一个函数，检查输入的链表是否是回文的。
+ */
